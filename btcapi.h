6756a435d454d0cffaad6e510da14718392b8c0f
Marco Scannadinari <marco@scannadinari.co.uk>
2013-11-18 21:15:43 +0000

    include stdint for uint32_t

--- btcapi.h
+++ btcapi.h
@@ -1,19 +1,19 @@
 /*
-	Copyright (C) 2013 Marco Scannadinari.
+	Copyright (C) 2013  Marco Scannadinari
 
 	This file is part of btcwatch.
 
 	btcwatch is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Lesser General Public License as published by
+	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
 	btcwatch is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Lesser General Public License for more details.
+	GNU General Public License for more details.
 
-	You should have received a copy of the GNU Lesser General Public License
+	You should have received a copy of the GNU General Public License
 	along with btcwatch.  If not, see <http://www.gnu.org/licenses/>.
 */
 
@@ -22,9 +22,13 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <wchar.h>
 
-#include "btcutil.h"
+typedef struct {
+	bool err;
+	char errstr[64];
+} btcerr_t;
 
 typedef struct {
 	char name[3 + 1];
@@ -53,7 +57,6 @@ bool fill_rates(const char *const currcy, btcerr_t *const api_err);
 // uses libcURL to access a Bitcoin API, calls write_data, then returns a JSON string
 
 char *get_json(const char *currcy, btcerr_t *const api_err);
-
 
 // uses jansson to parse the JSON string and returns a rates_t containing exchange information
 
