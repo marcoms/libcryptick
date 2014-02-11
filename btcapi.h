2c7944a089af2315046bc5cc5b0d74737325417f
Marco Scannadinari <m@scannadinari.co.uk>
2014-02-11 19:16:19 +0000

    replaced old btcwatch references with libbtcapi

--- btcapi.h
+++ btcapi.h
@@ -1,20 +1,20 @@
 /*
 	Copyright (C) 2013 Marco Scannadinari <m@scannadinari.co.uk>
 
-	This file is part of btcwatch.
+	This file is part of libbtcapi.
 
-	btcwatch is free software: you can redistribute it and/or modify
+	libbtcapi is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	btcwatch is distributed in the hope that it will be useful,
+	libbtcapi is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with btcwatch.  If not, see <http://www.gnu.org/licenses/>.
+	along with libbtcapi.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef BTCAPI_H
