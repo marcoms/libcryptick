4a2507fc0944ecf93fae4c1e1eba9cb04e7535e3
Marco Scannadinari <m@scannadinari.co.uk>
2014-02-23 15:16:52 +0000

    added and updated license statements

--- btcapi_currencies.h
+++ btcapi_currencies.h
@@ -1,3 +1,22 @@
+/*
+	Copyright (C) 2014 Marco Scannadinari <m@scannadinari.co.uk>
+
+	This file is part of libbtcapi.
+
+	libbtcapi is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	libbtcapi is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libbtcapi.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef BTCAPI_CURRENCIES_H
 #define BTCAPI_CURRENCIES_H
 
